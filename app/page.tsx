8145848d823761f4e84d86506a67330766078ce9
Eduardo Cobuci <educobuci@gmail.com>
2024-06-08 10:43:28 -0300

    Create local tracks

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,40 @@
-import Video from "twilio-video";
+'use client'
+
+import { useCallback, useEffect, useRef } from 'react'
+import {
+  LocalAudioTrack,
+  LocalVideoTrack,
+  createLocalTracks,
+} from 'twilio-video'
 
 export default function Home() {
-  return <main></main>;
+  const videoElement = useRef<HTMLVideoElement>(null)
+  const audioElement = useRef<HTMLAudioElement>(null)
+
+  const onMediaMount = useCallback(() => {
+    createLocalTracks({
+      audio: true,
+      video: true,
+    }).then((tracks) => {
+      const [localAudioTrack, localVideoTrack] = tracks as [
+        LocalAudioTrack,
+        LocalVideoTrack,
+      ]
+      localAudioTrack.attach(audioElement.current!)
+      localVideoTrack.attach(videoElement.current!)
+    })
+  }, [])
+
+  useEffect(() => {
+    if (videoElement.current && audioElement.current) {
+      onMediaMount()
+    }
+  }, [onMediaMount])
+
+  return (
+    <main>
+      <video ref={videoElement} className="-scale-x-100" autoPlay={true} />
+      <audio ref={audioElement} autoPlay={true} />
+    </main>
+  )
 }
